92647fdd2dee1020ff07ba889c77ba7d35156da5
wildy13 <wildy.simanjuntak13@gmail.com>
2024-07-03 09:03:39 +0700

    feat: add somethng in home page

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -6,11 +6,32 @@
                 <div class="flex justify-end row-start-1 col-start-3 row-end-6 col-end-4">
                     <img src="https://ecommerce-netlify.netlify.app/shoe1.jpg" alt="event-item" class="w-2/3 md:w-1/2">
                 </div>
-                <div class="font-montserrat text-[#efefef] row-start-3 col-start-1 row-end-5 col-end-5 text-[23vmin] md:text-[15vmin] xl:text-[30vmin] uppercase">
+                <div
+                    class="font-montserrat text-[#efefef] row-start-3 col-start-1 row-end-5 col-end-5 text-[23vmin] md:text-[15vmin] xl:text-[30vmin] uppercase">
                     New</div>
-                <div class="row-start-2 xl:row-start-1 col-start-2 row-end-3 col-end-4 text-[#d96528] text-4xl sm:text-5xl md:text-4xl xl:text-8xl">Men Shoes
+                <div
+                    class="row-start-2 xl:row-start-1 col-start-2 row-end-3 col-end-4 text-[#d96528] text-4xl sm:text-5xl md:text-4xl xl:text-8xl">
+                    Men Shoes
                 </div>
-                <div class="row-start-3 col-start-3 row-end-3 col-end-4  text-4xl sm:text-6xl md:text-5xl xl:text-9xl">Collection</div>
+                <div class="row-start-3 col-start-3 row-end-3 col-end-4  text-4xl sm:text-6xl md:text-5xl xl:text-9xl">
+                    Collection</div>
+            </div>
+            <div class="grid grid-cols-1 md:grid-cols-3 px-5 py-12 gap-4 font-montserrat">
+                <div class="border-2 h-60 flex flex-col justify-center items-center">
+                    <img src="https://ecommerce-netlify.netlify.app/icon-package.svg" alt="" class="w-32">
+                    <div class="uppercase font-bold text-lg">Free Sheeping Local</div>
+                    <div class="text-slate-400">Delivery in a day</div>
+                </div>
+                <div class="border-2 w-full h-60  flex flex-col justify-center items-center">
+                    <img src="https://ecommerce-netlify.netlify.app/icon-cal.svg" alt="" class="w-32">
+                    <div class="uppercase font-bold text-lg">24/7 Customer service</div>
+                    <div class="text-slate-400">Call us any time</div>
+                </div>
+                <div class="border-2 w-full h-60  flex flex-col justify-center items-center">
+                    <img src="https://ecommerce-netlify.netlify.app/icon-service.svg" alt="" class="w-32">
+                    <div class="uppercase font-bold text-lg">Easy online return</div>
+                    <div class="text-slate-400">Send within 30 days</div>
+                </div>
             </div>
         </section>
     </div>
